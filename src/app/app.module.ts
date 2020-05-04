f5a8f83a643aa0183161149397ea2f3fe3623987
Dheeraj <dheerajsaraogi25@gmail.com>
2020-05-04 14:38:07 +0530

    header feature section and employee section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
